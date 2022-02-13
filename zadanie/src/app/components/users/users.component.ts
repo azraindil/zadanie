2ea6ef9202e5b2f1075bf9ef3ed1432954f8eccd
Marta Kowalczyk <azraindil@gmail.com>
2022-02-13 16:41:30 +0100

    table loaded with users

--- zadanie/src/app/components/users/users.component.ts
+++ zadanie/src/app/components/users/users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { User } from '../../models/User';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public users:User[] =[];
+  private jsonUrl = 'assets\\test.json';
+ 
+  constructor(private http: HttpClient) {
+    
+    this.getJSON().subscribe(res=> {
+    this.users = res
+    console.log(this.users)
+  })
 }
+  
+  ngOnInit(): void {
+   
+  }
+ public getJSON(): Observable<any>{
+   return this.http.get(this.jsonUrl);
+ }
+}
+
---
+++ zadanie/src/app/models/User.ts
@@ -0,0 +1,9 @@
+export interface User{
+    id: string,
+    name: string,
+    gender: string,
+    balance: string,
+    company: string,
+    email: string,
+    phone: string
+}
